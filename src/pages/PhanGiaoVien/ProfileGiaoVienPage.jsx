620027de26a59677fc23ca64771b9dc0a3efd852
PGSTSHiepDuBai <nobitah2so4@gmail.com>
2025-06-09 14:50:32 +0700

    Page profile giáo viên

---
+++ src/pages/PhanGiaoVien/ProfileGiaoVienPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DashboardLayout } from '../../components/layout/DashboardLayout'
+import ProfileGiaoVien from '../../components/PhanGiaoVien/ProfileGiaoVien'
+
+
+export default function ProfileGiaoVienPage() {
+  return (
+    <DashboardLayout>
+      <ProfileGiaoVien/>
+    </DashboardLayout>
+  )
+}
