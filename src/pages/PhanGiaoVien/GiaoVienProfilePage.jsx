21cc5eb405188f2cecd56e8caa94276ae12dcdba
PGSTSHiepDuBai <nobitah2so4@gmail.com>
2025-06-14 17:03:15 +0700

    Đổi tên ProfileGiaoVien & ProfileGiaoVienPage

---
+++ src/pages/PhanGiaoVien/GiaoVienProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { DashboardLayout } from '../../components/layout/DashboardLayout'
+import { GiaoVienProfile } from '../../components/PhanGiaoVien/GiaoVienProfile'
+
+export default function GiaoVienProfilePage() {
+  return (
+    <DashboardLayout>
+      <GiaoVienProfile/>
+    </DashboardLayout>
+  )
+}
