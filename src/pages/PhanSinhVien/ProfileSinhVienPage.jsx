f4132cf2f5d000ec788fed694ec7ec13397e656c
PGSTSHiepDuBai <nobitah2so4@gmail.com>
2025-06-14 16:31:03 +0700

    Page profile sinh viên

---
+++ src/pages/PhanSinhVien/ProfileSinhVienPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { DashboardLayout } from '../../components/layout/DashboardLayout'
+import ProfileSinhVien from '../../components/PhanSinhVien/ProfileSinhVien'
+
+export default function ProfileSinhVienPage() {
+  return (
+    <DashboardLayout>
+        <ProfileSinhVien/>
+    </DashboardLayout>
+  )
+}
