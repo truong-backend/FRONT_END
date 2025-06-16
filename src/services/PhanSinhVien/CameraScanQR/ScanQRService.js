2d1b8ad67251e5805f92301a2ba7f578adbfe71a
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-06-17 03:38:10 +0700

    Đổi domain để truy vấn

--- src/services/PhanSinhVien/CameraScanQR/ScanQRService.js
+++ src/services/PhanSinhVien/CameraScanQR/ScanQRService.js
@@ -1,6 +1,5 @@
 import api from "../../api.js";
 
-const API_BASE_URL = 'http://localhost:8080/api/qrcode';
 
 export const ScanQRService = {
   // Điểm danh bằng QR Code
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "https://he-thong-diem-danh-stu.onrender.com/api";
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
