0b5f5a7cb0b0b01b5ae58920f2a8d0345374ae02
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-07-21 23:06:08 +0700

    erro path

--- src/pages/GiaoVien/ThemSinhVien/ThemSinhVienPage.jsx
+++ src/pages/GiaoVien/ThemSinhVien/ThemSinhVienPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ThemSinhVienComponents } from '../../../components/GiaoVien/ThemSinhVien/themSinhVienComponents.jsx';
+// import { ThemSinhVienComponents } from '../../../components/GiaoVien/ThemSinhVien/themSinhVienComponents.jsx';
 import { BocCucChinh } from '../../../components/BoCuc/BocCucChinh.jsx';
 import { Card } from 'antd';
 
 export const ThemSinhVienPage = () => {
   return (
     <BocCucChinh>
-        <ThemSinhVienComponents />
+        {/* <ThemSinhVienComponents /> */}
     </BocCucChinh>
   );
 };
