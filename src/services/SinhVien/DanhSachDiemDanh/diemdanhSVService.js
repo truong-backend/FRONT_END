2f372d1e40ac395d3e25a296c58c1ab3103a6f08
Tran Ngoc Hiep <nobitah2so4@gmail.com>
2025-08-02 11:48:36 +0700

    update service getDiemDanhByMonHoc truyền thêm nmh tránh trùng mã môn học

--- src/services/SinhVien/DanhSachDiemDanh/diemdanhSVService.js
+++ src/services/SinhVien/DanhSachDiemDanh/diemdanhSVService.js
@@ -3,6 +3,7 @@ import api from "../../api.js";
 export async function getMonHocBySinhVien(maSv) {
   try {
     const response = await api.get(`monhoc/danh-sach-mon-hoc-cua-sinh-vien?maSv=${maSv}`);
+    console.log('res mon hoc: ', response.data);
     return response.data;
   } catch (error) {
     console.error("Lỗi khi lấy danh sách môn học:", error);
@@ -10,9 +11,14 @@ export async function getMonHocBySinhVien(maSv) {
   }
 }
 
-export async function getDiemDanhByMonHoc(maMh) {
+export async function getDiemDanhByMonHoc(maMh, nhm) {
   try {
-    const response = await api.get(`/diemdanh/ketqua_diemdanh_sinhvien?maMh=${maMh}`);
+    const response = await api.get(`/diemdanh/ketqua_diemdanh_sinhvien`, {
+      params: {
+        maMh: maMh,
+        nmh: nhm
+      }
+    });
     console.log('res kqdd: ',response.data);
     console.log('res : ',response);
 
