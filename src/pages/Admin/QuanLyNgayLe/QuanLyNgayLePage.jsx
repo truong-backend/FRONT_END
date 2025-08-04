66a8499ef23b9dbdb824386132329a1f103cbb40
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-08-05 06:57:08 +0700

    role BE

--- src/pages/Admin/QuanLyNgayLe/QuanLyNgayLePage.jsx
+++ src/pages/Admin/QuanLyNgayLe/QuanLyNgayLePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BocCucChinh } from '../../../components/BoCuc/BocCucChinh.jsx';
 import {QuanLyNgayLeComponents} from '../../../components/QuanTriVien/QuanLyNgayLe/QuanLyNgayLeComponents.jsx'
 
-export const LichHocPage = () => {
+export const QuanLyNgayLePage = () => {
   return (
     <BocCucChinh>
       <QuanLyNgayLeComponents/>
